423011f13c6b94a0004c1d0b55c38767bad0b38d
Yonashenok <yonashenok24@gmail.com>
2023-10-30 16:44:10 +0300

    feat: set up linters

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import React, { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import Greeting from './components/Greeting';
+import Layout from './components/Layout';
+import { fetchMessage } from './redux/messages/messageSlice';
+
+function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMessage());
+  }, [dispatch]);
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Greeting />} />
+      </Route>
+    </Routes>
+  );
 }
+
+export default App;
